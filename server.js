4955b42e9f8e50d85c4af552d924e095a1b0dda7
Jakob <61665831+jakobloke@users.noreply.github.com>
2020-05-27 20:33:19 -0700

    added sql to express

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const sqlite3 = require('sqlite3').verbose();
+
+//Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//Connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+        return console.error(err.message);
+    }
+
+    console.log('Connected to the election database.');
+});
+
+db.all(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+// Default response for any other request(Not Found) Cath all
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+// Start server after DB connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
